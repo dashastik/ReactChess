46bc177572ef10164db6be67f2a055ecabe3eb1f
Daria <d.tsvetkova@storytelling-software.com>
2021-07-08 15:29:25 +0300

    Fix rendering figures

--- src/RenderingFigures/index.js
+++ src/RenderingFigures/index.js
@@ -7,54 +7,54 @@ const RenderingFigures = () => {
       <div className="figures__black">
         <div className="figure"
           onClick={() => gameManager.createPawn("black")}>
-          <i className="fas fa-chess-pawn figure-black"></i>
+          <i className="fas fa-chess-pawn set-figure figure-black"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createKnight("black")}>
-          <i className="fas fa-chess-knight figure-black"></i>
+          <i className="fas fa-chess-knight set-figure figure-black"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createBishop("black")}>
-          <i className="fas fa-chess-bishop figure-black"></i>
+          <i className="fas fa-chess-bishop set-figure figure-black"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createRook("black")}>
-          <i className="fas fa-chess-rook figure-black"></i>
+          <i className="fas fa-chess-rook set-figure figure-black"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createQueen("black")}>
-          <i className="fas fa-chess-queen figure-black"></i>
+          <i className="fas fa-chess-queen set-figure figure-black"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createKing("black")}>
-          <i className="fas fa-chess-king figure-black"></i>
+          <i className="fas fa-chess-king set-figure figure-black"></i>
         </div>
       </div>
 
       <div className="figures__white">
         <div className="figure"
           onClick={() => gameManager.createPawn("white")}>
-          <i className="fas fa-chess-pawn figure-white"></i>
+          <i className="fas fa-chess-pawn set-figure figure-white"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createKnight("white")}>
-          <i className="fas fa-chess-knight figure-white"></i>
+          <i className="fas fa-chess-knight set-figure figure-white"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createBishop("white")}>
-          <i className="fas fa-chess-bishop figure-white"></i>
+          <i className="fas fa-chess-bishop set-figure figure-white"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createRook("white")}>
-          <i className="fas fa-chess-rook figure-white"></i>
+          <i className="fas fa-chess-rook set-figure figure-white"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createQueen("white")}>
-          <i className="fas fa-chess-queen figure-white"></i>
+          <i className="fas fa-chess-queen set-figure figure-white"></i>
         </div>
         <div className="figure"
           onClick={() => gameManager.createKing("white")}>
-          <i className="fas fa-chess-king figure-white"></i>
+          <i className="fas fa-chess-king set-figure figure-white"></i>
         </div>
       </div>
 
