5173f9b964122dc8be589892fab3e7600f041e89
Daria <d.tsvetkova@storytelling-software.com>
2021-07-23 18:06:30 +0300

    Remake field in GameBoard

--- src/game/GameBoard/index.js
+++ src/game/GameBoard/index.js
@@ -3,25 +3,17 @@ import FieldDoesNotExist from "./../exceptions/FieldDoesNotExist"
 class GameBoard {
 
   constructor() {
-    this.field = [
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-    ]
+
+    this.field = new Array(8).fill(0).map(() => new Array(8).fill(0).map(() => null))
   }
 
   putFigure(figure, indexRow, indexColumn) {
-    
+
     if (indexRow < 0 || indexRow >= this.field.length) {
       throw new FieldDoesNotExist("indexRow non found")
     }
     if (indexColumn < 0 || indexColumn >= this.field[indexRow].length) {
-      throw new FieldDoesNotExist("indexColumn non found") 
+      throw new FieldDoesNotExist("indexColumn non found")
     }
     this.field[indexRow][indexColumn] = figure
   }
